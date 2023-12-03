bb71aee7e931f32cbd24466dd38a10740c25ae61
TaekyungHa <86tkstar@gmail.com>
2023-12-04 01:41:50 +0900

    modify Root UI

--- src/components/ContentWrapper.tsx
+++ src/components/ContentWrapper.tsx
@@ -4,8 +4,8 @@ import { useMenuState } from "@services/state/store/useMenuState.ts";
 export function ContentWrapper({ children }: PropsWithChildren) {
   const currentMenu = useMenuState((state) => state.currentMenu);
   return (
-    <div className="flex h-full w-full flex-col p-3 pt-20 lg:px-10">
-      <div className="box-content flex max-h-[40px] items-center justify-center border border-dashed border-neutral-900 px-3 lg:mb-10 lg:py-10">
+    <div className="flex h-full w-full flex-col p-3 pt-20 lg:px-10 lg:pt-10">
+      <div className="box-content flex max-h-[40px] items-center justify-center border border-dashed border-neutral-900 px-3 lg:mb-10 lg:py-5">
         <h2 className="text-3xl font-extrabold lg:text-5xl">
           {currentMenu?.name}
         </h2>
