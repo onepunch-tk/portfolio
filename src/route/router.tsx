ddd9fe8e1afa763e801360bf631afb91bcc0dec3
TaekyungHa <86tkstar@gmail.com>
2023-12-04 00:56:28 +0900

    add gh-pages

--- src/route/router.tsx
+++ src/route/router.tsx
@@ -6,15 +6,22 @@ import { AboutMeScreen } from "@pages/AboutMeScreen.tsx";
 import { ResumeScreen } from "@pages/ResumeScreen.tsx";
 import { ContactScreen } from "@pages/ContactScreen.tsx";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: ROOT_ROUTES.ROOT,
+      element: <RootScreen />,
+      children: [
+        { path: ROOT_ROUTES.ROOT, element: <HomeScreen /> },
+        { path: ROOT_ROUTES.ABOUT_ME, element: <AboutMeScreen /> },
+        { path: ROOT_ROUTES.RESUME, element: <ResumeScreen /> },
+        { path: ROOT_ROUTES.CONTACT, element: <ContactScreen /> },
+      ],
+    },
+  ],
   {
-    path: ROOT_ROUTES.ROOT,
-    element: <RootScreen />,
-    children: [
-      { path: ROOT_ROUTES.ROOT, element: <HomeScreen /> },
-      { path: ROOT_ROUTES.ABOUT_ME, element: <AboutMeScreen /> },
-      { path: ROOT_ROUTES.RESUME, element: <ResumeScreen /> },
-      { path: ROOT_ROUTES.CONTACT, element: <ContactScreen /> },
-    ],
-  },
-]);
+    ...(import.meta.env.VITE_DEPLOY_ROLE === "gh-pages" && {
+      basename: "/portfolio",
+    }),
+  }
+);
